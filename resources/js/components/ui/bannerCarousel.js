01ab13afeea024f993355acaef75f4c1c562ad0c
Severino <s.ammirati95@gmail.com>
2020-04-28 22:19:03 +0200

    - Banners

---
+++ resources/js/components/ui/bannerCarousel.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import Carousel from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+import img1 from './../../../../public/images/banner1.png'
+import img2 from './../../../../public/images/banner2.png'
+class BannerCarousel extends Component {
+    render() {
+        return (
+
+                <Carousel
+                    autoPlay={3000}
+                    stopAutoPlayOnHover
+                    infinite>
+                    <img onClick={() => window.open('https://www.facebook.com/Studio-Odontoiatrico-Castaldo-1505004786417059') } className="img-fluid" src={img1} />
+                    <img onClick={() => window.open('https://www.facebook.com/Streetdance-FSV-394427487946405')} className="img-fluid" src={img2} />
+
+                </Carousel>
+
+        );
+    }
+}
+
+export default BannerCarousel;
